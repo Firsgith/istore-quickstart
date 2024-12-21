8f6d90e5fac3b4bb32c9c87570c84498b0473b70
Firsgith <49604315+Firsgith@users.noreply.github.com>
2024-12-22 00:34:00 +0800

    Update quickstart.lua

--- luci-app-quickstart/luasrc/controller/quickstart.lua
+++ luci-app-quickstart/luasrc/controller/quickstart.lua
@@ -3,34 +3,22 @@ local http = require "luci.http"
 module("luci.controller.quickstart", package.seeall)
 
 function index()
-    -- First child for NAS
     entry({"admin", "nas"}, firstchild(), _("NAS"), 45).dependent = false
-    
-    -- Check if quickstart process is running
     if luci.sys.call("pgrep quickstart >/dev/null") == 0 then
-        -- QuickStart page
         entry({"admin", "quickstart"}, template("quickstart/home")).leaf = true
-        
-        -- Network guide pages
         entry({"admin", "network_guide"}, call("networkguide_index"), _("NetworkGuide"), 2)
         entry({"admin", "network_guide", "pages"}, call("quickstart_index", {index={"admin", "network_guide", "pages"}})).leaf = true
-        
-        -- Development page if available
         if nixio.fs.access("/usr/lib/lua/luci/view/quickstart/main_dev.htm") then
             entry({"admin", "quickstart_dev"}, call("quickstart_dev", {index={"admin", "quickstart_dev"}})).leaf = true
         end
-        
-        -- Additional NAS-related pages
         entry({"admin", "nas", "raid"}, call("quickstart_index", {index={"admin", "nas"}}), _("RAID"), 10).leaf = true
         entry({"admin", "nas", "smart"}, call("quickstart_index", {index={"admin", "nas"}}), _("S.M.A.R.T."), 11).leaf = true
         entry({"admin", "network", "interfaceconfig"}, call("quickstart_index", {index={"admin", "network"}}), _("NetworkPort"), 11).leaf = true
 
-        -- NAS quickstart entries
         entry({"admin", "nas", "quickstart"}).dependent = false
         entry({"admin", "nas", "quickstart", "auto_setup"}, post("auto_setup"))
         entry({"admin", "nas", "quickstart", "setup_result"}, call("setup_result"))
     else
-        -- Redirect fallback if quickstart process is not running
         entry({"admin", "quickstart"}, call("redirect_fallback")).leaf = true
     end
 end
@@ -43,7 +31,6 @@ function redirect_fallback()
     luci.http.redirect(luci.dispatcher.build_url("admin", "status"))
 end
 
--- Function to handle Vue.js language settings
 local function vue_lang()
     local i18n = require("luci.i18n")
     local lang = i18n.translate("quickstart_vue_lang")
@@ -53,23 +40,20 @@ local function vue_lang()
     return lang
 end
 
--- Render the quickstart page
 function quickstart_index(param)
     luci.template.render("quickstart/main", {prefix=luci.dispatcher.build_url(unpack(param.index)), lang=vue_lang()})
 end
 
--- Render the quickstart development page
 function quickstart_dev(param)
     luci.template.render("quickstart/main_dev", {prefix=luci.dispatcher.build_url(unpack(param.index)), lang=vue_lang()})
 end
 
--- Handle auto setup
 function auto_setup()
     local os = require "os"
     local fs = require "nixio.fs"
     local rshift = nixio.bit.rshift
 
-    -- Retrieve package list from form data
+    -- form style
     local packages = luci.http.formvalue("packages")
     local pkgs = ""
     if type(packages) == "table" then
@@ -84,7 +68,6 @@ function auto_setup()
         end
     end
 
-    -- Execute auto setup script
     local ret
     if pkgs == "" then
         ret = {
@@ -117,7 +100,6 @@ function auto_setup()
     luci.http.write_json(ret)
 end
 
--- Get setup result
 function setup_result()
     local fs = require "nixio.fs"
     local taskd = require "luci.model.tasks"
@@ -126,7 +108,7 @@ function setup_result()
     local failed = nil
     local status = taskd.status("auto_setup")
     local ret = {}
-    
+
     if status.running or status.exit_code ~= 404 then
         local item
         local po = fs.readfile("/var/log/auto_setup.input") or ""
